7dd88b60b082a1ea3f7a0ced44943cb87eb6a0fc
Théo FIDRY <theo.fidry@gmail.com>
2022-07-08 23:56:52 +0200

    Fix forwarded command does not work on first install (#103)
    Closes #72
    
    The issue was that `PluginEvents::COMMAND` is not triggered at all on the first install. The trick done here is to hook on the post dump autoload instead. This requires a few hack:
    
    - extend `ConsoleIO` to expose the input & output
    - ensure we are not forwarding the command twice
    
    But I think overall this delivers a better user experience.

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,13 +8,18 @@ use Bamarni\Composer\Bin\CommandProvider as BamarniCommandProvider;
 use Composer\Composer;
 use Composer\Console\Application;
 use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\ConsoleIO;
 use Composer\IO\IOInterface;
 use Composer\Plugin\CommandEvent;
 use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
 use Composer\Plugin\Capable;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
 use Symfony\Component\Console\Command\Command;
 use Composer\Plugin\Capability\CommandProvider as ComposerPluginCommandProvider;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
 use function in_array;
 use function sprintf;
@@ -40,6 +45,8 @@ class Plugin implements PluginInterface, Capable, EventSubscriberInterface
      * @var Logger
      */
     private $logger;
+
+    private $forwarded = false;
 
     public function activate(Composer $composer, IOInterface $io): void
     {
@@ -67,31 +74,76 @@ class Plugin implements PluginInterface, Capable, EventSubscriberInterface
     {
         return [
             PluginEvents::COMMAND => 'onCommandEvent',
+            ScriptEvents::POST_AUTOLOAD_DUMP => 'onPostAutoloadDump',
         ];
+    }
+
+    public function onPostAutoloadDump(Event $event): void
+    {
+        $this->logger->logDebug('Calling onPostAutoloadDump().');
+
+        $eventIO = $event->getIO();
+
+        if (!($eventIO instanceof ConsoleIO)) {
+            return;
+        }
+
+        // This is a bit convoluted but Event does not expose the input unlike
+        // CommandEvent.
+        $publicIO = PublicIO::fromConsoleIO($eventIO);
+        $eventInput = $publicIO->getInput();
+
+        $this->onEvent(
+            $eventInput->getArgument('command'),
+            $eventInput,
+            $publicIO->getOutput()
+        );
     }
 
     public function onCommandEvent(CommandEvent $event): bool
     {
         $this->logger->logDebug('Calling onCommandEvent().');
 
+        return $this->onEvent(
+            $event->getCommandName(),
+            $event->getInput(),
+            $event->getOutput()
+        );
+    }
+
+    private function onEvent(
+        string $commandName,
+        InputInterface $input,
+        OutputInterface $output
+    ): bool {
         $config = Config::fromComposer($this->composer);
 
         if ($config->isCommandForwarded()
-            && in_array($event->getCommandName(), self::FORWARDED_COMMANDS, true)
+            && in_array($commandName, self::FORWARDED_COMMANDS, true)
         ) {
-            return $this->onForwardedCommand($event);
+            return $this->onForwardedCommand($input, $output);
         }
 
         return true;
     }
 
-    protected function onForwardedCommand(CommandEvent $event): bool
-    {
-        $this->logger->logDebug('The command is being forwarded.');
+    protected function onForwardedCommand(
+        InputInterface $input,
+        OutputInterface $output
+    ): bool {
+        if ($this->forwarded) {
+            $this->logger->logDebug('Command already forwarded within the process: skipping.');
+
+            return true;
+        }
+
+        $this->forwarded = true;
+
+        $this->logger->logStandard('The command is being forwarded.');
         $this->logger->logDebug(
             sprintf(
                 'Original input: <comment>%s</comment>.',
-                $event->getInput()
+                $input
             )
         );
 
@@ -99,19 +151,19 @@ class Plugin implements PluginInterface, Capable, EventSubscriberInterface
         // input & output.
         $io = $this->io;
 
+        $application = new Application();
+
         $command = new BinCommand();
         $command->setComposer($this->composer);
+        $command->setApplication($application);
         $command->setIO($io);
-        $command->setApplication(new Application());
 
-        $forwardedCommandInput = BinInputFactory::createForwardedCommandInput(
-            $event->getInput()
-        );
+        $forwardedCommandInput = BinInputFactory::createForwardedCommandInput($input);
 
         try {
             return Command::SUCCESS === $command->run(
                 $forwardedCommandInput,
-                $event->getOutput()
+                $output
             );
         } catch (Throwable $throwable) {
             return false;
