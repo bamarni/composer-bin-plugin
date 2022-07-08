269a9c70bad47a4f811d471a41c7e6c0aafc8394
Théo FIDRY <theo.fidry@gmail.com>
2022-07-08 09:11:24 +0200

    Add end-to-end tests (#95)

---
+++ e2e/scenario1/script.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+readonly ORIGINAL_WORKING_DIR=$(pwd)
+
+trap "cd ${ORIGINAL_WORKING_DIR}" err exit
+
+# Change to script directory
+cd "$(dirname "$0")"
+
+# Ensure we have a clean state
+rm -rf actual.txt || true
+rm -rf composer.lock || true
+rm -rf vendor || true
+rm -rf vendor-bin/*/composer.lock || true
+rm -rf vendor-bin/*/vendor || true
+
+composer update
+
+# Actual command to execute the test itself
+composer bin all update --verbose 2>&1 | tee > actual.txt
---
+++ tests/EndToEndTest.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bamarni\Composer\Bin\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Process\Process;
+use function basename;
+use function dirname;
+use function file_get_contents;
+use function getcwd;
+use function preg_replace;
+use function realpath;
+use function sprintf;
+use function str_replace;
+use function trim;
+
+/**
+ * @group e2e
+ */
+final class EndToEndTest extends TestCase
+{
+    private const E2E_DIR = __DIR__.'/../e2e';
+
+    /**
+     * @dataProvider scenarioProvider
+     */
+    public function test_it_passes_the_e2e_test(string $scenarioPath): void
+    {
+        $scenarioProcess = new Process(
+            ['./script.sh'],
+            $scenarioPath,
+            null,
+            null,
+            10
+        );
+
+        $expected = file_get_contents($scenarioPath.'/expected.txt');
+
+        $scenarioProcess->run();
+
+        self::assertTrue(
+            $scenarioProcess->isSuccessful(),
+            $scenarioProcess->getOutput().'|'.$scenarioProcess->getErrorOutput()
+        );
+
+        $actual = self::retrieveActualOutput(
+            getcwd(),
+            $scenarioPath
+        );
+        ;
+
+        self::assertSame($expected, $actual);
+    }
+
+    public static function scenarioProvider(): iterable
+    {
+        $scenarios = Finder::create()
+            ->files()
+            ->depth(1)
+            ->in(self::E2E_DIR)
+            ->name('README.md');
+
+        foreach ($scenarios as $scenario) {
+            $scenarioPath = dirname($scenario->getPathname());
+            $scenarioName = basename($scenarioPath);
+            $description = trim($scenario->getContents());
+
+            $title = sprintf(
+                '[%s] %s',
+                $scenarioName,
+                $description
+            );
+
+            yield $title => [
+                realpath($scenarioPath),
+            ];
+        }
+    }
+
+    private static function retrieveActualOutput(
+        string $cwd,
+        string $scenarioPath
+    ): string {
+        $originalContent = file_get_contents($scenarioPath.'/actual.txt');
+
+        $normalizedContent = str_replace(
+            $cwd,
+            '/path/to/project',
+            $originalContent
+        );
+
+        // Sometimes in the CI a different log is shown, e.g. in https://github.com/bamarni/composer-bin-plugin/runs/7246889244
+        $normalizedContent = preg_replace(
+            '/> command: .+\n/',
+            '',
+            $normalizedContent
+        );
+
+        // Those values come from the expected.txt, it actually does matter how
+        // many they are at instant t.
+        $normalizedContent = preg_replace(
+            '/Analyzed (\d+) packages to resolve dependencies/',
+            'Analyzed 90 packages to resolve dependencies',
+            $normalizedContent
+        );
+        $normalizedContent = preg_replace(
+            '/Analyzed (\d+) rules to resolve dependencies/',
+            'Analyzed 90 rules to resolve dependencies',
+            $normalizedContent
+        );
+
+        return $normalizedContent;
+    }
+}
