b50c1fea4cb1dc54049596d6054efb12cae8c364
Théo FIDRY <theo.fidry@gmail.com>
2022-07-10 15:41:53 +0200

    Add

---
+++ e2e/scenario7/script.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+readonly ORIGINAL_WORKING_DIR=$(pwd)
+
+trap "cd ${ORIGINAL_WORKING_DIR}" err exit
+
+# Change to script directory
+cd "$(dirname "$0")"
+
+# Ensure we have a clean state
+rm -rf actual.txt || true
+rm -rf composer.lock || true
+rm -rf vendor || true
+rm -rf vendor-bin/*/composer.lock || true
+rm -rf vendor-bin/*/vendor || true
+
+composer update
+
+# Actual command to execute the test itself
+composer bin ns1 update --no-dev --verbose -- foo 2>&1 | tee > actual.txt || true
+echo "–––––––––––––––––––––" >> actual.txt
+composer bin ns1 show --direct --name-only 2>&1 | tee >> actual.txt || true
