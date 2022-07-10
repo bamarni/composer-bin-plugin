468af9dbe17bd5566ebf1888438c8312d6ec11fd
Théo FIDRY <theo.fidry@gmail.com>
2022-07-10 15:55:58 +0200

    Add env variables to e2e tests to make the tests more portable (#114)

--- e2e/scenario2/script.sh
+++ e2e/scenario2/script.sh
@@ -2,6 +2,12 @@
 
 set -Eeuo pipefail
 
+# Set env envariables in order to experience a behaviour closer to what happens
+# in the CI locally. It should not hurt to set those in the CI as the CI should
+# contain those values.
+export CI=1
+export COMPOSER_NO_INTERACTION=1
+
 readonly ORIGINAL_WORKING_DIR=$(pwd)
 
 trap "cd ${ORIGINAL_WORKING_DIR}" err exit
