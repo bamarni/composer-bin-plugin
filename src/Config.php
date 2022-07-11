c85113bb7ef82c375ce84487a7c9afe39ee7e135
Théo FIDRY <theo.fidry@gmail.com>
2022-07-11 09:31:33 +0200

    Add deprecations

--- src/Config.php
+++ src/Config.php
@@ -5,14 +5,47 @@ declare(strict_types=1);
 namespace Bamarni\Composer\Bin;
 
 use Composer\Composer;
+use UnexpectedValueException;
 use function array_merge;
+use function function_exists;
+use function gettype;
+use function is_bool;
+use function is_string;
+use function sprintf;
 
 final class Config
 {
+    private const EXTRA_CONFIG_KEY = 'bamarni-bin';
+
+    private const BIN_LINKS = 'bin-links';
+    private const TARGET_DIRECTORY = 'target-directory';
+    private const FORWARD_COMMAND = 'forward-command';
+
+    private const DEFAULT_CONFIG = [
+        self::BIN_LINKS => true,
+        self::TARGET_DIRECTORY => 'vendor-bin',
+        self::FORWARD_COMMAND => false,
+    ];
+
     /**
-     * @var array{'bin-links': bool, 'target-directory': string, 'forward-command': bool}
+     * @var bool
      */
-    private $config;
+    private $binLinks;
+
+    /**
+     * @var string
+     */
+    private $targetDirectory;
+
+    /**
+     * @var bool
+     */
+    private $forwardCommand;
+
+    /**
+     * @var list<string>
+     */
+    private $deprecations = [];
 
     public static function fromComposer(Composer $composer): self
     {
@@ -24,28 +57,93 @@ final class Config
      */
     public function __construct(array $extra)
     {
-        $this->config = array_merge(
-            [
-                'bin-links' => true,
-                'target-directory' => 'vendor-bin',
-                'forward-command' => false,
-            ],
-            $extra['bamarni-bin'] ?? []
+        $config = array_merge(
+            self::DEFAULT_CONFIG,
+            $extra[self::EXTRA_CONFIG_KEY] ?? []
         );
+
+        $getType = function_exists('get_debug_type') ? 'get_debug_type' : 'gettype';
+
+        $binLinks = $config[self::BIN_LINKS];
+
+        if (!is_bool($binLinks)) {
+            throw new UnexpectedValueException(
+                sprintf(
+                    'Expected setting "%s.%s" to be a boolean value. Got "%s".',
+                    self::EXTRA_CONFIG_KEY,
+                    self::BIN_LINKS,
+                    $getType($binLinks)
+                )
+            );
+        }
+
+        if ($binLinks) {
+            $this->deprecations[] = sprintf(
+                'The setting "%s.%s" will be set to "false" from 2.x onwards. If you wish to keep it to "true", you need to set it explicitly.',
+                self::EXTRA_CONFIG_KEY,
+                self::BIN_LINKS
+            );
+        }
+
+        $targetDirectory = $config[self::TARGET_DIRECTORY];
+
+        if (!is_string($targetDirectory)) {
+            throw new UnexpectedValueException(
+                sprintf(
+                    'Expected setting "%s.%s" to be a string. Got "%s".',
+                    self::EXTRA_CONFIG_KEY,
+                    self::TARGET_DIRECTORY,
+                    $getType($targetDirectory)
+                )
+            );
+        }
+
+        $forwardCommand = $config[self::FORWARD_COMMAND];
+
+        if (!is_bool($forwardCommand)) {
+            throw new UnexpectedValueException(
+                sprintf(
+                    'Expected setting "%s.%s" to be a boolean value. Got "%s".',
+                    self::EXTRA_CONFIG_KEY,
+                    self::FORWARD_COMMAND,
+                    gettype($forwardCommand)
+                )
+            );
+        }
+
+        if (!$forwardCommand) {
+            $this->deprecations[] = sprintf(
+                'The setting "%s.%s" will be set to "true" from 2.x onwards. If you wish to keep it to "false", you need to set it explicitly.',
+                self::EXTRA_CONFIG_KEY,
+                self::BIN_LINKS
+            );
+        }
+
+        $this->binLinks = $binLinks;
+        $this->targetDirectory = $targetDirectory;
+        $this->forwardCommand = $forwardCommand;
     }
 
     public function binLinksAreEnabled(): bool
     {
-        return true === $this->config['bin-links'];
+        return $this->binLinks;
     }
 
     public function getTargetDirectory(): string
     {
-        return $this->config['target-directory'];
+        return $this->targetDirectory;
     }
 
     public function isCommandForwarded(): bool
     {
-        return $this->config['forward-command'];
+        return $this->forwardCommand;
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function getDeprecations(): array
+    {
+        return $this->deprecations;
     }
 }
