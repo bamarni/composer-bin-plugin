9f1e0c7076af682ff65f5a9a5159b9ec4d305579
Théo FIDRY <theo.fidry@gmail.com>
2024-11-05 10:37:31 +0100

    test: Add check to ensure the right version of Composer is used

---
+++ tests/ComposerVersionProviderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bamarni\Composer\Bin\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function preg_match;
+use function sprintf;
+
+/**
+ * @group e2e
+ * @covers \Bamarni\Composer\Bin\Tests\ComposerVersionProvider
+ */
+final class ComposerVersionProviderTest extends TestCase
+{
+    public function test_it_can_get_the_composer_version(): void
+    {
+        $expectedPattern = '/^\d+\.\d+\.\d+$/';
+
+        $actual = ComposerVersionProvider::getComposerVersion();
+
+        self::assertSame(
+            1,
+            preg_match($expectedPattern, $actual),
+            sprintf(
+                'The version "%s" does not match the pattern "%s".',
+                $actual,
+                $expectedPattern
+            )
+        );
+    }
+}
